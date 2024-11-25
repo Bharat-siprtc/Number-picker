22d492606b707389227d8399e2bd0c09f0096811
Bharat Prajapati <bharat@o365.siprtc.io>
2024-11-25 12:09:13 +0000

    condition change

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 let count=1;
 let btnVisible;
 function incrementNo() {
-    count+=1;
     if (count==1) {
         // currentVal+=1;
         document.getElementById("btn-dec").disabled=false;
     }
+    count+=1;
     document.getElementById("display").textContent=count;
 }
 function decrementNo() {
